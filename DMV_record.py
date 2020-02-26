7bc609319f5adf1e59c9a8af9494552095435159
thortrygg <61520677+thortrygg@users.noreply.github.com>
2020-02-26 13:00:44 -0800

    Create DMV_record.py

---
+++ DMV_record.py
@@ -0,0 +1,98 @@
+import DMVAPP
+import pickle
+
+class dmvapp():
+	
+	def __init__(self):
+		
+		def command_loop(self):
+			return input("1: List Vehicles\n 2:Display Vehicle Record\n 3: Add Vehicle\n 4: Remove Vehicle\n 5:Load DB\n 6: Save DB\n 7: Remove all Vehicles\n 0: Exit")
+				
+	class dmvappl():
+		def __init__(self, command_loop):
+			command_loop = cmdloop
+		
+		def cmdloop(self):
+			"""lists the possible inputs"""
+			cmdloop = input("1: List Vehicles\n 2:Display Vehicle Record\n 3: Add Vehicle\n 4: Remove Vehicle\n 5:Load DB\n 6: Save DB\n 7: Remove all Vehicles\n 0: Exit")
+	
+input(" 1: List Vehicles\n 2:Display Vehicle Record\n 3: Add Vehicle\n 4: Remove Vehicle\n 5:Load DB\n 6: Save DB\n 7: Remove all Vehicles\n 0: Exit\n")
+while 1:
+	print(DMVdatabase)
+	answer = input("Press 9 to go back")
+	if answer in ('9'):
+		for x in DMVdatabase:
+			print(f'Car# {counter}\n')
+			print (DMVdatabase[counter-1])
+			counter = counter+ 1
+
+while 2:
+	
+	answer = input("Press 9 to go back")
+	if answer in ('9'):
+		for x in DMVdatabase:
+			print(f'Car# {counter}\n')
+			print (DMVdatabase[counter-1])
+			counter = counter+ 1
+
+while 3:
+	carrecord = dmvcarrecord (input('Enter Plate #: '),input('Enter Vehicle Make: '),input('Enter Vehicle Model: '), input('Enter Vehicle Year: '), input('Enter Owner Name: '), input('Registration Expiration: '))
+	DMVdatabase.append (carrecord) 
+	answer = input("Press 9 to go back")
+	if answer in ('9'):
+		for x in DMVdatabase:
+			print(f'Car# {counter}\n')
+			print (DMVdatabase[counter-1])
+			counter = counter+ 1
+			
+while 4:
+	print(DMVcarrecord)
+	input ("select vehicle to delete")
+	answer = input("Press 9 to go back")
+	if answer in ('9'):
+		for x in DMVdatabase:
+			print(f'Car# {counter}\n')
+			print (DMVdatabase[counter-1])
+			counter = counter+ 1
+			
+while 5:
+	print(DMVapp)
+	input ("select database to load")
+		while 1:
+		if answer in ('1')
+			remove car1
+			car1.DMVcarrecord.append
+		while 2:
+		if answer in ('2')
+			remove car2
+			car2.DMVcarrecord.append
+		while 3:
+		if answer in ('3')
+			remove car3
+			car3.DMVcarrecord.append
+		while 4:
+		if answer in ('4')
+			remove car4
+			car4.DMVcarrecord.append
+		while 5:	
+		if answer in ('5')
+			remove car5
+			car5.DMVcarrecord.append
+		while 6:
+		if answer in ('6')
+			remove car6
+			car6.DMVcarrecord.append
+		while 7:
+		if answer in ('7')
+			remove car7
+			car7.DMVcarrecord.append
+DMVcarrecord.append
+
+while 6:
+	DMVcarrecord.append
+
+while 7:
+	clear_cache.DMVcarrecord.DMVAPP
+	
+while 0:
+	close()
